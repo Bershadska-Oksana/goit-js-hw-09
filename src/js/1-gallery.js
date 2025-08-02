fb2141996dce77fa1849075fe206c257066e3180
Oksana Bershadska <ksenaua@icloud.com>
2025-08-02 16:40:25 +0200

    save changes

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,6 +1,3 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
 const images = [
   {
     preview:
@@ -72,21 +69,22 @@ const galleryContainer = document.querySelector('.gallery');
 const galleryMarkup = images
   .map(
     ({ preview, original, description }) => `
-    <li class="gallery-item">
-      <a class="gallery-link" href="${original}">
-        <img class="gallery-image"
-             src="${preview}"
-             data-source="${original}"
-             alt="${description}" />
-      </a>
-    </li>
-  `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+          <img
+            class="gallery-image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `
   )
   .join('');
 
-galleryEl.innerHTML = galleryMarkup;
+galleryContainer.innerHTML = galleryMarkup;
 
-new SimpleLightbox('.gallery a', {
+const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionPosition: 'bottom',
   captionDelay: 250,
